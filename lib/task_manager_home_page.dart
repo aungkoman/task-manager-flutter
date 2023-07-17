eaf306f5d0f04a0c47900e88d4ac8efcac42cf57
Aung Ko Man <aungkoman@gmail.com>
2023-07-17 13:15:15 +0630

    ui layouted

--- lib/task_manager_home_page.dart
+++ lib/task_manager_home_page.dart
@@ -10,6 +10,43 @@ class TaskManagerHomePage extends StatefulWidget {
 class _TaskManagerHomePageState extends State<TaskManagerHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Task Manager"),
+      ),
+      body: _mainWidget(),
+    );
+  }
+
+  Widget _mainWidget(){
+    return Column(
+      children: [
+        _inputRow(),
+
+        Expanded(
+          child: ListView.separated(
+              itemBuilder: (context, index) => _taskCard(),
+              separatorBuilder: (context, index) => Divider(),
+              itemCount: 10
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _inputRow(){
+    return Row(
+      children: [
+        Expanded(child: TextFormField()),
+        ElevatedButton(onPressed: (){}, child: Text("Create Task")),
+      ],
+    );
+  }
+
+  Widget _taskCard(){
+    return Container(
+      height: 50,
+      color: Colors.green,
+    );
   }
 }
