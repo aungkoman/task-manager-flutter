89922af6c017a75c6ded1ef5ad1fd08b18cd28ac
Aung Ko Man <aungkoman@gmail.com>
2023-07-17 13:08:32 +0630

    initial commit

---
+++ android/app/src/main/kotlin/com/mmsoftware100/taskmanager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mmsoftware100.taskmanager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/task_manager_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskManagerHomePage extends StatefulWidget {
+  const TaskManagerHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<TaskManagerHomePage> createState() => _TaskManagerHomePageState();
+}
+
+class _TaskManagerHomePageState extends State<TaskManagerHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
